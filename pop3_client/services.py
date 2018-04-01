5909eef3a6883917b214f073e8693f7677a80718
Vasil <v.slavov96@gmail.com>
2018-04-01 18:26:55 +0300

    Add list and retr handlers

--- pop3_client/services.py
+++ pop3_client/services.py
@@ -1,4 +1,5 @@
 from message import Message
+from sys_utils import read_message_list_file, read_message_file
 
 
 def handle_user(msg: str):
@@ -8,17 +9,26 @@ def handle_user(msg: str):
     message = 'user accepted'
 
     if not authenticated:
-        message = "authentication failed"
+        message = 'authentication failed'
 
-    return Message(success=authenticated, message=message)
-
-
-def handle_pass(msg: str):
-    pass
+    return Message(success=authenticated, message=message), 'user'
 
 
-def handle_list(msg: str):
-    pass
+def handle_pass(msg: str, user):
+    if user is None:  # or authentication from Django fails
+        return Message(success=False, message='authentication failed')
+
+    return Message(success=True,
+                   message='password accepted, connection established')
+
+
+def handle_list(msg: str, user):
+    message_list = read_message_list_file(f'{user}_list.txt')
+    messages = '\n'.join(message_list)
+
+    return_message = f'Mailbox scan list follows\n{messages}'
+
+    return Message(success=True, message=return_message)
 
 
 def handle_top(msg: str):
@@ -26,7 +36,14 @@ def handle_top(msg: str):
 
 
 def handle_retr(msg: str):
-    pass
+    try:
+        message_id = int(msg.split(' ')[-1])
+        contents = read_message_file(f'{message_id}.txt')
+
+        return Message(success=True,
+                       message=f'{len(contents)} octets\n{contents}')
+    except ValueError:
+        return Message(success=False, message='Message file not found')
 
 
 def handle_dele(msg: str):
--- pop3_client/sys_utils.py
+++ pop3_client/sys_utils.py
@@ -2,6 +2,10 @@ import os
 
 
 def read_init_file() -> str:
+    """
+    Reads the file with requests and returns the last request
+    """
+
     init_fd = os.open('init_message.txt', os.O_RDONLY)
 
     while init_fd == -1:
@@ -12,6 +16,59 @@ def read_init_file() -> str:
     if len(read_bytes) == 0:
         read_init_file()
 
-    os.close(fd)
+    os.close(init_fd)
 
-    return read_bytes.decode('utf-8').split('\n')[0]
+    return read_bytes.decode('utf-8').split('\n')[-1]
+
+
+def read_message_list_file(filepath: str) -> str:
+    """
+    Reads all contents of a message file and returns a list of the messages
+    """
+    message_fd = os.open(filepath, os.O_RDONLY)
+
+    while message_fd == -1:
+        message_fd = os.open(filepath, os.O_RDONLY)
+
+    end_reached = False
+    messagefile_contents = ''
+
+    while not end_reached:
+        read_bytes = os.read(message_fd, 4096)
+
+        if len(read_bytes) < 1:
+            end_reached = True
+            break
+
+        messagefile_contents += read_bytes.decode('utf-8')
+
+    os.close(message_fd)
+
+    return messagefile_contents.split('\n')
+
+
+def read_message_file(filepath: str):
+    message_fd = os.open(filepath, os.O_RDONLY)
+
+    retry_count = 0
+
+    while message_fd == -1:
+        if retry_count == 11:
+            raise ValueError('Message file not found')
+
+        message_fd = os.open(filepath, os.O_RDONLY)
+        retry_count += 1
+
+    end_reached = False
+    messagefile_contents = ''
+
+    while not end_reached:
+        read_bytes = os.read(message_fd, 4096)
+
+        if len(read_bytes) < 1:
+            end_reached = True
+            break
+
+        messagefile_contents += read_bytes.decode('utf-8')
+
+    return messagefile_contents
