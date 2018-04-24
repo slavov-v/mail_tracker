47865ef8b28cc0e5cbd755094b59be00b38e06e8
Vasil <v.slavov96@gmail.com>
2018-04-24 17:44:57 +0300

    Add retr and dele stats

--- mail_tracker_web/web/management/commands/run_stats.py
+++ mail_tracker_web/web/management/commands/run_stats.py
@@ -11,13 +11,13 @@ def time_run(func):
         result = func(*args, **kwargs)
         end = timezone.now()
 
-        print(f'{args} {end - start}')
+        print(f'{" ".join(args)} {end - start}')
         return result
     return wrapper
 
 
 @time_run
-def run_list(username):
+def run_list_stats(file_size, message_count, username):
     with EstablishConnection() as connection:
         connection.send(settings.POP3_LIST_COMMAND.format(username).encode('utf-8'))
         data = connection.receive()
@@ -31,8 +31,49 @@ def run_list(username):
     return context
 
 
+@time_run
+def run_retr_stats(file_size, id):
+    with EstablishConnection() as connection:
+        connection.send(settings.POP3_RETRIEVE_COMMAND.format(id).encode('utf-8'))
+        data = connection.receive()
+
+        context = {
+            'received_data': data.decode('utf-8'),
+            'message_id': id
+        }
+
+    return context
+
+
+@time_run
+def run_dele_stats(file_size, id):
+    with EstablishConnection() as connection:
+        connection.send(settings.POP3_DELETE_COMMAND.format(
+            'test',
+            id).encode('utf-8'))
+
+
+@time_run
+def run_save_stats(message_size, message):
+    with EstablishConnection() as connection:
+        connection.send(settings.POP3_SAVE_COMMAND.format(message).encode('utf-8'))
+
+
 class Command(BaseCommand):
     def handle(self, *args, **options):
-        usernames = ['test', 'test1', 'test2', 'test3']
-        for username in usernames:
-            run_list(username)
+        usernames = ['test', 'test1', 'test2', 'test3', 'test4']
+        file_sizes = ['231', '569', '909', '2691', '4510']
+        message_count = ['30', '70', '100', '300', '500']
+
+        for index, username in enumerate(usernames):
+            run_list_stats(f'LIST Size:{file_sizes[index]}', f'Messages:{message_count[index]}', username=username)
+
+        file_sizes = [100, 500, 1000, 2000, 4000, 8000, 15000, 50000, 150000, 300000]
+        for i in range(1, 11):
+            run_retr_stats(f'RETR Size: {file_sizes[i-1]}', id=i)
+
+        for index, i in enumerate(range(2001, 2011)):
+            run_dele_stats(f'DELE Size: {file_sizes[index]}', id=i)
+
+        for index, message in enumerate():
+            pass
